9020646b453d92964af6e60d7dd8629c2f055301
gavindeiss <gavindeiss3@gmail.com>
2024-01-01 13:09:53 -0500

    Revert "onClick is now doing something (something stupid but something)"
    This reverts commit e25b189496a95a067bbb03be1743aba10368c6cb.

--- client/song-sort/src/components/Playlist/Playlist.js
+++ client/song-sort/src/components/Playlist/Playlist.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 
-export const Playlist = ({playlistPayload, onPlaylistClick}) => {
+export const Playlist = ({playlistPayload}) => {
     return (
         <div className="d-flex m-2 align-items-center"
              style={{ cursor: "pointer" }}
              key={playlistPayload.key}
-             onClick={() => onPlaylistClick(playlistPayload)} 
+             // onClick={handlePlay} 
         >
             <img src={playlistPayload.imageUrl} style={{ height: "64px", width: "64px" }} />
             <div className="ml-3" style={{ paddingLeft: "10px" }}>
